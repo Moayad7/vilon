4c6bfbbab419ca97e9b2bc5a2dfcbfd2d11fbe4d
Moayad7 <mouayad.ahmad7@gmail.com>
2024-05-16 15:44:26 +0300

    1

--- js/main.js
+++ js/main.js
@@ -41,16 +41,37 @@ window.addEventListener('scroll', function() {
   });
 
 
-  const swiper2 = new Swiper('.swiper-container2', {
+
+  const swiper2 = new Swiper('.swiper-container1', {
     slidesPerView: 2.5,
-    spaceBetween: 50,
-    loop: true,
+    spaceBetween: 10,
+    mousewheel: {
+      forceToAxis: true,
+    },
+    // loop: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
     autoplay: {
-      delay: 1500,
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
+
+  const swiper3 = new Swiper('.swiper-container2', {
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+    mousewheel: {
+      forceToAxis: true,
+    },
+    // loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 4000,
       disableOnInteraction: false,
     },
   });
